6eb11710d47c09bf1c8c70ca15162af0420ca580
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-06-21 18:41:09 -0500

    add from 4 to 8

--- JavaScript/2) Fundamentos I/fundamentosI.js
+++ JavaScript/2) Fundamentos I/fundamentosI.js
@@ -27,3 +27,59 @@ function f3() {
   return sum;
 }
 console.log(f3());
+
+// 4) sum array
+function f4(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+console.log(f4([-5, 2, 5, 12]));
+
+// 5) max array value
+function f5(arr) {
+  var max = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(f5([-3, 3, 5, 7]));
+
+// 6) average array value
+function f6(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(f6([1, 3, 5, 7, 20]));
+
+// 7) odd array
+function f7() {
+  var arr = [];
+  for (var i = 1; i <= 50; i++) {
+    if (i % 2 === 1) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+console.log(f7());
+
+// 8) greater than
+function f8(arr, Y) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > Y) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(f8([1, 3, 5, 7], 3));
