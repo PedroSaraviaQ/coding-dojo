1c077dc95244f9707c577f0b520732669023f362
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-06-21 23:22:04 -0500

    add more algos

--- JavaScript/3) Fundamentos II/fundamentosII.js
+++ JavaScript/3) Fundamentos II/fundamentosII.js
@@ -9,6 +9,29 @@ function f1(arr) {
 }
 console.log(f1([-1, 3, 5, -5]));
 
+// 4) double
+function f4(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArr[i] = arr[i] * 2;
+  }
+  return newArr;
+}
+console.log(f4([1, 2, 3]));
+
+// 5) count positives
+function f5(arr) {
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      count++;
+    }
+  }
+  arr[arr.length - 1] = count;
+  return arr;
+}
+console.log(f5([-1, 1, 1, 1]));
+
 // 9) add 7
 function f9(arr) {
   var newArr = [];
@@ -40,3 +63,28 @@ function f11(arr) {
   return arr;
 }
 console.log(f11([1, -3, 5]));
+
+// 12) yum yum
+function f12(arr) {
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "food") {
+      console.log("yummy");
+      count++;
+    }
+  }
+  if (count === 0) {
+    console.log("I'm hungry");
+  }
+}
+f12([1, 5, "food", 2, "food"]);
+f12([4, true]);
+
+// 14) multiply
+function f14(arr, Y) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] *= Y;
+  }
+  return arr;
+}
+console.log(f14([1, 2, 3], 3));
