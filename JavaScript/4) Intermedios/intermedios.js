b01dccfbbe0700c1a59b9851c32f3690af6c9461
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-06-22 10:06:15 -0500

    add more algos

--- JavaScript/4) Intermedios/intermedios.js
+++ JavaScript/4) Intermedios/intermedios.js
@@ -11,6 +11,15 @@ function factorial(num) {
 }
 console.log(factorial(5));
 
+// 3)
+function fibonacci(n) {
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(fibonacci(7));
+
 // 4)
 function secondToLast(arr) {
   if (arr[arr.length - 2] !== undefined) {
@@ -19,3 +28,39 @@ function secondToLast(arr) {
   return null;
 }
 console.log(secondToLast([42, true, 4, "Liam", 7]));
+
+// 5)
+function returnN(arr, n) {
+  if (arr[n] !== undefined) {
+    return arr[n];
+  }
+  return null;
+}
+console.log(returnN([5, 2, 3, 6, 4, 9, 7], 3));
+
+// 6)
+function secondBiggest(arr) {
+  var max1 = arr[0];
+  var max2 = 0;
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > max1) {
+      max2 = max1;
+      max1 = arr[i];
+    } else if (arr[i] > max2) {
+      max2 = arr[i];
+    }
+  }
+  return max2;
+}
+console.log(secondBiggest([42, 1, 4, 3.14, 7]));
+
+// 7)
+function double(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push(arr[i]);
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+console.log(double([4, "Ulysses", 42, false]));
