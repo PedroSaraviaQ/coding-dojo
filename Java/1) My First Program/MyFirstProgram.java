77688bbd68960e0251c47427d1b1c4dcd9f6a38a
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-07-09 14:11:30 -0500

    add my first program

---
+++ Java/1) My First Program/MyFirstProgram.java
@@ -0,0 +1,7 @@
+class MyFirstProgram{
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Pedro");
+        System.out.println("Tengo 22 años de edad");
+        System.out.println("Mi ciudad es Lima, Perú");
+    }
+}
