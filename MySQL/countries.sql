cca0b63e613fc057b129092cc7f0efd2a8c61c9e
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-07-09 12:50:23 -0500

    add 2nd countries

--- MySQL/countries.sql
+++ MySQL/countries.sql
@@ -4,6 +4,13 @@ use world;
 
 select countries.name, languages.language, languages.percentage
 from countries join languages
-on countries.code=languages.country_code
+on countries.code = languages.country_code
 where language = 'Slovene'
 order by languages.percentage desc;
+
+-- * 2) Número de ciudades por país
+
+select countries.name, count(cities.name)
+from countries join cities
+on countries.id = cities.country_id
+group by countries.name
