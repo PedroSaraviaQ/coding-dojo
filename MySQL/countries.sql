10cda0a62b815f7a535a98c3c66f2e081327e0be
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-07-09 13:10:08 -0500

    add 3, 4 and some fixes

--- MySQL/countries.sql
+++ MySQL/countries.sql
@@ -4,13 +4,28 @@ use world;
 
 select countries.name, languages.language, languages.percentage
 from countries join languages
-on countries.code = languages.country_code
-where language = 'Slovene'
+on countries.id = languages.country_id
+where languages.language = 'Slovene'
 order by languages.percentage desc;
 
 -- * 2) Número de ciudades por país
 
-select countries.name, count(cities.name)
+select countries.name, count(cities.name) as total
 from countries join cities
 on countries.id = cities.country_id
 group by countries.name
+order by total desc;
+
+-- * 3) Ciudades de México con población mayor a 500,000 habitantes
+
+select name, population from cities
+where country_id = 136 and population > 500000
+order by population desc;
+
+-- * 4) Idiomas en países con un porcentaje mayor al 89%
+
+select countries.name, languages.language, languages.percentage
+from countries join languages
+on countries.id = languages.country_id
+where languages.percentage > 89
+order by languages.percentage desc
