b4d13d7f92b92c170fb9fe2ebd9841eb4e99f343
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-07-20 16:09:36 -0500

    add pitagoras

---
+++ Java/_2. Pitagoras/PitagorasTest.java
@@ -0,0 +1,7 @@
+public class PitagorasTest {
+    public static void main(String[] args) {
+        Pitagoras pitagoras=new Pitagoras();
+        double hipotenusa=pitagoras.calcularHipotenusa(3, 4);
+        System.out.println(hipotenusa);
+    }
+}
