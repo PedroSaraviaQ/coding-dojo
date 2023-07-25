5748515f4bd1d0c6a4ee09cfd27d4581d4156461
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-07-25 12:37:45 -0500

    add more basicjava

--- Java/_5. Basic Java/BasicJava.java
+++ Java/_5. Basic Java/BasicJava.java
@@ -19,6 +19,20 @@ public class BasicJava {
         System.out.println(obtenerPromedio(nums3));
         //* 7)
         System.out.println(arregloImpares());
+        //* 8)
+        int[] nums4 = {1, 3, 5, 7};
+        System.out.println(mayorQue(nums4, 3));
+        //* 9)
+        int[] nums5 = {1, 5, 10, -2};
+        System.out.println(valoresCuadrado(nums5));
+        //* 10)
+        int[] nums6 = {1, 5, 10, -2};
+        System.out.println(numerosNegativos(nums6));
+        //* 11)
+        System.out.println(minimoMaximoPromedio(nums6));
+        //* 12)
+        int[] nums7 = {1, 5, 10, 7, -2};
+        System.out.println(cambiarValoresArreglo(nums7));
     }
 
     static void imprimir1A255() {
@@ -75,5 +89,64 @@ public class BasicJava {
             }
         }
         return impares;
+    }
+
+    static int mayorQue(int[] arr, int Y) {
+        int nums = 0;
+        for (int element : arr) {
+            if (element > Y) {
+                nums++;
+            }
+        }
+        return nums;
+    }
+
+    static ArrayList<Integer> valoresCuadrado(int[] arr) {
+        ArrayList<Integer> cuadrados = new ArrayList<Integer>();
+        for (int element : arr) {
+            cuadrados.add(element * element);
+        }
+        return cuadrados;
+    }
+
+    static ArrayList<Integer> numerosNegativos(int[] arr) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                nums.add(arr[i]);
+            } else {
+                nums.add(0);
+            }
+        }
+        return nums;
+    }
+
+    static ArrayList<Object> minimoMaximoPromedio(int[] arr) {
+        ArrayList<Object> result = new ArrayList<Object>();
+        int min = arr[0];
+        int max = arr[0];
+        int sum = 0;
+        for (int element : arr) {
+            if (element < min) {
+                min = element;
+            }
+            if (element > max) {
+                max = element;
+            }
+            sum += element;
+        }
+        result.add(min);
+        result.add(max);
+        result.add((double) sum / arr.length);
+        return result;
+    }
+
+    static ArrayList<Integer> cambiarValoresArreglo(int[] arr) {
+        ArrayList<Integer> new_arr = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length - 1; i++) {
+            new_arr.add(arr[i + 1]);
+        }
+        new_arr.add(0);
+        return new_arr;
     }
 }
