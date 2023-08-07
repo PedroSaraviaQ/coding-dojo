4026791199197b84adb61bcd4b29ad4868f45232
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-08-07 13:40:59 -0500

    add project

---
+++ Java/_9. Class Project/ProjectTest.java
@@ -0,0 +1,16 @@
+public class ProjectTest {
+    public static void main(String[] args) {
+        Project project_1 = new Project();
+        project_1.setName("Zoom");
+        project_1.setDescription("A video conferencing app");
+        project_1.setInitialCost(1000.00);
+        System.out.println(project_1.elevatorPitch());
+
+        Project project_2 = new Project("Slack");
+        System.out.println(project_2.getName());
+
+        Project project_3 = new Project("Google", "A search engine");
+        project_3.setDescription("A search engine and more");
+        System.out.println(project_3.getDescription());
+    }
+}
