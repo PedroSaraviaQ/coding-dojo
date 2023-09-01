69e9aef21faa5321cdfeb46c4d6948532ca38b3c
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-08-31 20:55:27 -0500

    remove tvdb

--- TVDB/src/main/java/com/example/tvdb/repositories/ProgramRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tvdb.repositories;
-
-import com.example.tvdb.models.Program;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProgramRepository extends BaseRepository<Program> {
-    Boolean existsByTitle(String title);
-}
